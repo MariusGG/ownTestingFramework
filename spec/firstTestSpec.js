aa2fda9e89e7319ebb8044e66b88632d5d4c63e3
mariusgg <marius.g.guei@gmail.com>
2018-11-14 12:41:34 +0000

    updating assert.js

--- spec/firstTestSpec.js
+++ spec/firstTestSpec.js
@@ -1,10 +1,8 @@
-(function(exports) {
+
 
   function fizzBuzz() {
     var fizzBuzz = new FizzBuzz("Test fizzBuzz");
     assert.isTrue(fizzBuzz.number === "FizzBuzz", 'number is equal to fizzbuzz');
   };
 
-  fizzBuzz();
-
-})(this);
+  fizzBuzz()
